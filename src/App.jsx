dc6a90b8cef8ed285cb27320a9c3c5749195bc50
JFedeFernandez <jofedefe93@gmail.com>
2024-12-24 17:07:45 -0300

    modificamos rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import NavBar from './components/NavBar.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { ProductsProvider } from './context/ProductContext.jsx';
-import ItemListContainer from './components/ItemListContainer.jsx';
-import ItemDetailContainer from './components/ItemDetailContainer.jsx';
-import Cart from './components/Cart.jsx';
+import ItemListContainer from './screens/ItemListContainer/ItemListContainer.jsx';
+import ItemDetailContainer from './screens/ItemDetailContainer/ItemDetailContainer.jsx';
+import Cart from './screens/Cart/Cart.jsx';
 import { CartProvider } from './context/CartContext.jsx';
-import CheckoutForm from './components/CheckoutForm.jsx';
+import CheckoutForm from './screens/CheckoutForm/CheckoutForm.jsx';
 
 function App() {
   return (
