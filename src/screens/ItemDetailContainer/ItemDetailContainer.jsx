01a6d8ba38916ff450f94fc2c21a0a3a7c078c2d
JFedeFernandez <jofedefe93@gmail.com>
2024-12-24 17:00:38 -0300

    carpetas screens creadas

---
+++ src/screens/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,11 @@
+import ItemDetail from '../../components/ItemDetail/ItemDetail.jsx';
+import './styled.css';
+
+export default function ItemDetailContainer() {
+
+    return (
+        <section className='itemDetailContainer'>
+            <ItemDetail/>
+        </section>
+    );
+};
---
+++ src/screens/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import ItemList from '../../components/ItemList/ItemList.jsx';
+import './styled.css';
+
+export default function ItemListContainer() {
+  return (
+    <section className='itemListContainer'>
+      <ItemList/>
+    </section>
+  )
+}
